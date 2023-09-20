4c0a0dcfb9792cd030c99d0218b4e724217cc342
Novitz Jean-Philippe <novitz@gmail.com>
2023-09-20 23:11:20 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      // "./src/**/*.php",
+      "./assets/**/*.js",
+      "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
