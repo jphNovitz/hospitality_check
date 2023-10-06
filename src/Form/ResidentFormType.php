60898056e3f2c1153e404fe31638b7251b84ba93
Novitz Jean-Philippe <novitz@gmail.com>
2023-10-06 05:52:18 +0200

    Add Resident form  Type/_form

--- src/Form/ResidentFormType.php
+++ src/Form/ResidentFormType.php
@@ -3,15 +3,27 @@
 namespace App\Form;
 
 use App\Entity\Resident;
-use App\Form\EventListener\AddRoomSubscriber;
+use App\Entity\Room;
+use App\EventListener\AddRoomSubscriber;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ResidentFormType extends AbstractType
 {
+    private $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -19,11 +31,15 @@ class ResidentFormType extends AbstractType
             ->add('firstName')
             ->add('birthDate')
             ->add('nationality')
-            ->add('room')
+            ->add('room', EntityType::class, [
+                'class'=> Room::class,
+                'required'   => false,
+                'empty_data' => '',
+            ])
             ->add('newRoom', TextType::class, ['mapped' => false])
             ->add('referent');
 
-        $builder->addEventSubscriber(new AddRoomSubscriber());
+        $builder->addEventSubscriber(new AddRoomSubscriber($this->entityManager));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
