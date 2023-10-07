96819e051421d03c6145b0c2a42ed67e628ee6d7
Novitz Jean-Philippe <novitz@gmail.com>
2023-10-07 05:28:19 +0200

    Refactor Resident Form Type
    Add Event Subscriber that add a room if field is filled

---
+++ src/EventListener/AddRoomSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Room;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class AddRoomSubscriber implements EventSubscriberInterface
+{
+    private $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+// Tells the dispatcher that you want to listen on the form.pre_set_data
+// event and that the preSetData method should be called.
+        return [FormEvents::PRE_SUBMIT => 'preSubmit'];
+//return [FormEvents::PRE_SET_DATA => 'preSetData'];
+    }
+
+    public function preSubmit(FormEvent $event): void
+    {
+
+        $resident = $event->getData();
+        $form = $event->getForm();
+//        dd($resident);
+//        dd(empty($resident['newRoom']));
+
+        if (!empty($resident['newRoom'])) {
+            if (!$this->entityManager->getRepository(Room::class)->findOneBy(['id' => $resident['newRoom']])) {
+                $room = new Room();
+                $room->setNumber($resident['newRoom']);
+                $this->entityManager->persist($room);
+                $this->entityManager->flush();
+                $id = $room->getId();
+                $resident['room'] = $id;
+            }
+        }
+        $event->setData($resident);
+
+    }
+}
---
+++ src/Form/ResidentType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Resident;
+use App\Entity\Room;
+use App\EventListener\AddRoomSubscriber;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResidentType extends AbstractType
+{
+    private $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('picture')
+            ->add('firstName')
+            ->add('birthDate')
+            ->add('nationality')
+            ->add('room', EntityType::class, [
+                'class'=> Room::class,
+                'required'   => false,
+                'empty_data' => '',
+            ])
+            ->add('newRoom', TextType::class, ['mapped' => false])
+            ->add('referent');
+
+        $builder->addEventSubscriber(new AddRoomSubscriber($this->entityManager));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Resident::class,
+        ]);
+    }
+}
