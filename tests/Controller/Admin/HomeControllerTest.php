5e7bd1166ce6ece2ef68c7f275a4e33bb7f5c11d
Novitz Jean-Philippe <novitz@gmail.com>
2023-10-09 02:26:14 +0200

    Add TEST ADMIN Home Controller Test

---
+++ tests/Controller/Admin/HomeControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Tests\Controller\Admin;
+
+use App\DataFixtures\Tests\ResidentTestFixtures;
+use App\DataFixtures\Tests\RoomTestFixtures;
+use App\DataFixtures\Tests\UserTestFixtures;
+use App\Entity\Resident;
+use App\Entity\User;
+use App\Repository\ResidentRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private UserRepository $user_repository;
+    private ResidentRepository $resident_repository;
+    private string $path = '/admin/';
+    private EntityManagerInterface $manager;
+    private mixed $databaseTool;
+
+
+    /**
+     * @throws \Exception
+     */
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->resident_repository = static::getContainer()->get('doctrine')->getRepository(Resident::class);
+        $this->user_repository = static::getContainer()->get('doctrine')->getRepository(User::class);
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
+    }
+
+    public function test_home_admin_is_not_accessible_if_not_admin(): void
+    {
+        $this->databaseTool->loadFixtures([UserTestFixtures::class]);
+        $user = $this->user_repository->find(2);
+        $this->client->loginUser($user);
+
+        $this->client->request('GET', '/admin/home');
+
+        self::assertResponseStatusCodeSame(403);
+        self::assertTrue( $user->getRoles() === ['ROLE_USER']);
+    }
+
+    public function test_home_admin_is_not_accessible_if__admin(): void
+    {
+        $this->databaseTool->loadFixtures([
+            UserTestFixtures::class,
+//            RoomTestFixtures::class,
+//            ResidentTestFixtures::class,
+        ]);
+        $user = $this->user_repository->find(1);
+        $this->client->loginUser($user);
+
+        $crawler = $this->client->request('GET', '/admin/home');
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertResponseIsSuccessful();
+        self::assertContains('ROLE_ADMIN', $user->getRoles());
+//        $this->assertStringContainsString('home admin ', $crawler->text());
+    }
+}
