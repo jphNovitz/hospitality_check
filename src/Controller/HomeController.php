fe3d326c03b2018b3910cee7a283c6298e6f8fcf
Novitz Jean-Philippe <novitz@gmail.com>
2023-10-11 00:26:20 +0200

    refactor HomeController, HomeControllerTest
    redirect to admin home if user is admin
    redirect to resident home if user is user
    dont redirect if user is not verified

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,15 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if ($user = $this->getUser()) {
+            if (null !== $user->getName()) {
+                if ($user->isVerified())
+                    if (in_array('ROLE_ADMIN', $user->getRoles())) return $this->redirectToRoute('app_admin_home');
+                    else return $this->redirectToRoute('app_resident_index');
+            } else {
+                return $this->render('welcome.html.twig', []);
+            }
+        }
         return $this->render('welcome.html.twig', []);
     }
 }
--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -2,16 +2,87 @@
 
 namespace App\Tests\Controller;
 
+use App\DataFixtures\Tests\UserTestFixtures;
+use App\Entity\Resident;
+use App\Entity\User;
+use App\Repository\ResidentRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    private KernelBrowser $client;
+    private UserRepository $user_repository;
+    private string $path = '/';
+    private EntityManagerInterface $manager;
+    private mixed $databaseTool;
+
+
+    /**
+     * @throws \Exception
+     */
+    protected function setUp(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $this->client = static::createClient();
+        $this->resident_repository = static::getContainer()->get('doctrine')->getRepository(Resident::class);
+        $this->user_repository = static::getContainer()->get('doctrine')->getRepository(User::class);
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
+    }
+
+    public function test_homepage_is_accessible(): void
+    {
+        $crawler = $this->client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Bienvenue sur Roomate');
     }
+
+    public function test_homepage_redirect_to_admin_home_if_user_is_admin(){
+        $this->databaseTool->loadFixtures([UserTestFixtures::class]);
+
+        $admin_user = $this->user_repository->find(1);
+        $this->client->loginUser($admin_user);
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertContains('ROLE_ADMIN', $admin_user->getRoles());
+        $this->assertTrue($admin_user->isVerified());
+        $this->assertResponseStatusCodeSame(302);
+        $this->assertResponseRedirects('/admin/home');
+
+    }
+
+    public function test_homepage_redirect_to_resident_if_user_is_user(){
+        $this->databaseTool->loadFixtures([UserTestFixtures::class]);
+
+        $user = $this->user_repository->find(2);
+        $this->client->loginUser($user);
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertContains('ROLE_USER', $user->getRoles());
+        $this->assertTrue($user->isVerified());
+        $this->assertResponseStatusCodeSame(302);
+        $this->assertResponseRedirects('/resident/');
+
+    }
+
+    public function test_homepage_redirect_dont_redirect_if_user_is_not_verified()
+    {
+        $this->databaseTool->loadFixtures([UserTestFixtures::class]);
+
+        $user = $this->user_repository->find(3);
+        $this->client->loginUser($user);
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertContains('ROLE_USER', $user->getRoles());
+        $this->assertFalse($user->isVerified());
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(200);
+
+    }
+
+
 }
