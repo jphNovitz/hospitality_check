596189d526bf903bb5bfa872bdbe7adec9715093
Novitz Jean-Philippe <novitz@gmail.com>
2023-10-15 23:58:01 +0200

    refactor BaseControllerTest.php

---
+++ tests/Controller/BaseControllerTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Base;
+use App\Repository\BaseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BaseControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private BaseRepository $repository;
+    private string $path = '/preference/base/';
+    private EntityManagerInterface $manager;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = static::getContainer()->get('doctrine')->getRepository(Base::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Base index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'base[name]' => 'Testing',
+            'base[description]' => 'Testing',
+            'base[icon]' => 'Testing',
+            'base[resident]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/preference/base/');
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Base();
+        $fixture->setName('My Title');
+        $fixture->setDescription('My Title');
+        $fixture->setIcon('My Title');
+        $fixture->setResident('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Base');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Base();
+        $fixture->setName('My Title');
+        $fixture->setDescription('My Title');
+        $fixture->setIcon('My Title');
+        $fixture->setResident('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'base[name]' => 'Something New',
+            'base[description]' => 'Something New',
+            'base[icon]' => 'Something New',
+            'base[resident]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/preference/base/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getName());
+        self::assertSame('Something New', $fixture[0]->getDescription());
+        self::assertSame('Something New', $fixture[0]->getIcon());
+        self::assertSame('Something New', $fixture[0]->getResident());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new Base();
+        $fixture->setName('My Title');
+        $fixture->setDescription('My Title');
+        $fixture->setIcon('My Title');
+        $fixture->setResident('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+        self::assertResponseRedirects('/preference/base/');
+    }
+}
