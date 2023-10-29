6fc2ab35036af981b35fce6fff282b3309b54a4d
Novitz Jean-Philippe <novitz@gmail.com>
2023-10-30 00:23:50 +0100

    Add interest stimuls js
    - manage add form row onclick
    - manage rempve row onclick

--- assets/controllers/interest_controller.js
+++ assets/controllers/interest_controller.js
@@ -10,15 +10,38 @@ import { Controller } from '@hotwired/stimulus';
  */
 export default class extends Controller {
 
-    static targets = [ 'button', 'prototype' ]
+    static targets = [ 'button', 'remove', 'prototype' ]
     connect() {
         console.log('interest works !')
 
     }
 
     add(event){
-        console.log("click")
-        console.log(prototypeTarget.dataset.prototype)
         event.preventDefault()
+        console.log("click")
+        let prototype = this.prototypeTarget.dataset.prototype
+        let index = this.prototypeTarget.dataset.index
+        let newForm = prototype.replace(/__name__/g, index)
+        let row = document.createElement('span')
+        this.prototypeTarget.dataset.index++
+        row.innerHTML = newForm
+        let wrapper = document.getElementsByClassName('js-interest-wrapper')[0]
+        wrapper.insertBefore(row, this.buttonTarget)
+        // wrapper.innerHTML += newForm
+        console.log(wrapper)
+
+    }
+    remove(event){
+        event.preventDefault()
+        console.log("click remove")
+        let target = event.target
+        // console.log(this.removeTarget.parentNode)
+        let prototype = this.prototypeTarget.dataset.prototype
+        let index = this.prototypeTarget.dataset.index
+        this.prototypeTarget.dataset.index--
+        let wrapper = document.getElementsByClassName('js-interest-wrapper')[0]
+        // wrapper.remove(this.removeTarget)
+        target.parentNode.remove()
+
     }
 }
