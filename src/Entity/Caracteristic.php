c99677a04e00d3e90c5375c274f640f192cf8809
Novitz Jean-Philippe <novitz@gmail.com>
2023-11-10 01:00:34 +0100

    Add Caracteristic Entity, add

---
+++ src/Entity/Caracteristic.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CaracteristicRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CaracteristicRepository::class)]
+class Caracteristic
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $priority = null;
+
+    #[ORM\ManyToOne(inversedBy: 'characteristics')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Resident $resident = null;
+
+    public function __tostring(){
+        return $this->name;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): static
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getPriority(): ?int
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(?int $priority): static
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    public function getResident(): ?Resident
+    {
+        return $this->resident;
+    }
+
+    public function setResident(?Resident $resident): static
+    {
+        $this->resident = $resident;
+
+        return $this;
+    }
+}
