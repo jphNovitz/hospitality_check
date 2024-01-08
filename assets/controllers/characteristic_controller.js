8e0c622d456963a42166fcb6f64e39a92e4f043b
Novitz Jean-Philippe <novitz@gmail.com>
2024-01-08 15:21:48 +0100

    Add Stimulus characteristic_controller.js

---
+++ assets/controllers/characteristic_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from '@hotwired/stimulus';
+
+/*
+ * This Controller manage rows in characteristics form
+ * Add and remove action
+ * using form prototype from symfony
+ *
+ */
+export default class extends Controller {
+
+    static targets = [ 'button', 'remove', 'prototype' ]
+    connect() {
+        console.log('characteristics works !')
+
+    }
+
+    add(event){
+        event.preventDefault()
+
+        let prototype = this.prototypeTarget.dataset.prototype
+        let index = this.prototypeTarget.dataset.index
+        let newRow = prototype.replace(/__name__/g, index)
+        let row = document.createElement('div')
+        let wrapper = document.getElementsByClassName('js-characteristic-wrapper')[0]
+
+        this.prototypeTarget.dataset.index++
+
+        row.innerHTML = newRow
+        console.log(wrapper)
+        console.log(row)
+        wrapper.insertBefore(row, this.buttonTarget)
+    }
+    remove(event){
+        event.preventDefault()
+
+        let target = event.target
+        target.parentNode.remove()
+
+        // this.prototypeTarget.dataset.index--
+
+    }
+}
