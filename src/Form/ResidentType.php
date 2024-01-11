8e3846084fd08a835fd028ae988443ef827a4e62
Novitz Jean-Philippe <novitz@gmail.com>
2024-01-12 00:14:18 +0100

    Refactor: Resident Form Type
    Remove commented code

--- src/Form/ResidentType.php
+++ src/Form/ResidentType.php
@@ -2,23 +2,16 @@
 
 namespace App\Form;
 
-use App\Entity\Base;
 use App\Entity\Resident;
 use App\Entity\Room;
 use App\EventListener\AddRoomSubscriber;
-use App\EventListener\ManageReferentSubscriber;
 use App\EventSubscriber\ResidentSubscriber;
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\Nullable;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -29,7 +22,7 @@ class ResidentType extends AbstractType
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        private TokenStorageInterface $tokenStorage
+        private readonly TokenStorageInterface $tokenStorage
     )
     {
         $this->entityManager = $entityManager;
@@ -59,45 +52,6 @@ class ResidentType extends AbstractType
                 'required'   => false,
             ])
             ->add('referent')
-
-
-            /*
-            ->add('bases', EntityType::class, [
-                'class' => Base::class,
-                'choice_label' => 'name',
-                'multiple' => true,
-                'expanded' => true,
-                'required' => false,
-                'by_reference' => false,
-            ])*/
-
-
-//            ->add('interests', CollectionType::class, [
-//                'entry_type' => InterestType::class,
-//                'entry_options' => [
-//                    'label' => false,
-//                ],
-//                'allow_add' => true,
-//                'allow_delete' => true,
-//                'prototype' => true,
-//                'by_reference' => false,
-//
-//            ])
-
-
-           /* ->add('characteristics', CollectionType::class, [
-                'entry_type' => CharacteristicType::class,
-                'entry_options' => [
-                    'label' => false,
-                ],
-                'allow_add' => true,
-                'allow_delete' => true,
-                'prototype' => true,
-                'by_reference' => false,
-
-
-            ])*/
-
         ;
 
 
