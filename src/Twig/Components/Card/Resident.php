84f5bf2e58a492e301a7518587bc24a5acd1741b
Novitz Jean-Philippe <novitz@gmail.com>
2024-01-14 23:34:45 +0100

    Fix: Resident Card compoennt
    fix/change template path

--- src/Twig/Components/Card/Resident.php
+++ src/Twig/Components/Card/Resident.php
@@ -4,7 +4,7 @@ namespace App\Twig\Components\Card;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent(template: 'components/Card/Resident/Resident.html.twig')]
+#[AsTwigComponent(template: 'components/Card/Resident.html.twig')]
 final class Resident
 {
     public object $resident;
