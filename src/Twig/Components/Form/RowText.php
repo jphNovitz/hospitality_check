f59db844a9324bf91e9b9f2359f8d5c858b32a10
Novitz Jean-Philippe <novitz@gmail.com>
2024-07-29 00:58:22 +0200

    Refactor residents element (admin and common)
    - refactor new page
    - buttons
    - add icons
    - form

--- src/Twig/Components/Form/RowText.php
+++ src/Twig/Components/Form/RowText.php
@@ -9,6 +9,7 @@ final class RowText
 {
     public mixed $row;
     public ?string $label = '';
+    public ?string $placeholder = 'Entrez du texte ici';
 //    public function mount(mixed $row = null){
 //
 //    }
