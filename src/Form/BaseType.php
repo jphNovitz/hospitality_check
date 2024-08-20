580ef5c735efe0e4b8531f1f5d91175c97c2c7ab
Novitz Jean-Philippe <novitz@gmail.com>
2024-08-20 15:32:59 +0200

    Refactor baseType
    Make image not required

--- src/Form/BaseType.php
+++ src/Form/BaseType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Base;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -13,9 +15,9 @@ class BaseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('imageFile', VichImageType::class)
+            ->add('name', TextType::class, ['label' => 'Name'])
+            ->add('description', TextareaType::class, ['required' => false])
+            ->add('imageFile', VichImageType::class, ['required' => false])
         ;
     }
 
