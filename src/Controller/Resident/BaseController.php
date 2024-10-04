f1aaded6106bb91693cfd7f42e4e19fd83bedc54
Novitz Jean-Philippe <novitz@gmail.com>
2024-10-04 05:46:58 +0200

    Update resident route to use slug instead of id
    The route for the resident base edit has been modified to use a slug parameter instead of an id. This change ensures better readability and potentially more SEO-friendly URLs.

--- src/Controller/Resident/BaseController.php
+++ src/Controller/Resident/BaseController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/resident/{id}/base', name: 'app_resident_base_edit', methods: ['GET', 'POST'])]
+#[Route('/resident/{slug}/base', name: 'app_resident_base_edit', methods: ['GET', 'POST'])]
 final class BaseController extends AbstractController
 {
     public function __invoke(Request $request, Resident $resident, EntityManagerInterface $entityManager): Response
